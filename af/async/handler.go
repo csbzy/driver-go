b1b1a1df0bad1435464394a07b59f79bbf34d61d
chenshaobo <chenshaobo@sunwoda.com>
2024-09-29 17:22:20 +0800

    init

---
+++ af/async/handler.go
@@ -0,0 +1,38 @@
+package async
+
+import (
+	"sync"
+
+	"github.com/taosdata/driver-go/v3/wrapper/handler"
+)
+
+const defaultPoolSize = 10000
+
+var (
+	HandlerPool *handler.HandlerPool
+	once        = sync.Once{}
+)
+
+func SetHandlerSize(size int) {
+	once.Do(func() {
+		HandlerPool = handler.NewHandlerPool(size)
+	})
+}
+
+func GetHandler() *handler.Handler {
+	//if HandlerPool == nil {
+	//	SetHandlerSize(defaultPoolSize)
+	//}
+	return HandlerPool.Get()
+}
+
+func PutHandler(h *handler.Handler) {
+	if HandlerPool == nil {
+		return
+	}
+	HandlerPool.Put(h)
+}
+
+func init() {
+	HandlerPool = handler.NewHandlerPool(defaultPoolSize)
+}
